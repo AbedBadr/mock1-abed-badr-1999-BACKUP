1d48c88101af1bae6459b82d73ab989e248ac480
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-10-14 23:12:08 +0200

    Add files via upload

---
+++ TCPClient/TCPClient/NewTCPClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient
+{
+    class NewTCPClient
+    {
+        static void Main1(string[] args)
+        {
+            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            Console.WriteLine("Client ready");
+
+            Stream ns = clientSocket.GetStream();  //provides a Stream
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    string message = Console.ReadLine();
+            //    sw.WriteLine(message);
+
+            //    string serverAnswer = sr.ReadLine();
+
+            //    Console.WriteLine("Server: " + serverAnswer);
+            //}
+
+            Console.WriteLine("No more from server. Press Enter");
+            Console.ReadLine();
+
+            ns.Close();
+            clientSocket.Close();
+        }
+    }
+}
