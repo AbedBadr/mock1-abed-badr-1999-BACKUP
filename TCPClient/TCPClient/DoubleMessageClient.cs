4e3eac78a97e488af53b924acf92e972b2d30a69
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-10-17 01:38:31 +0200

    Add files via upload

---
+++ TCPClient/TCPClient/DoubleMessageClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPClient
+{
+    class DoubleMessageClient
+    {
+        static void Main1(string[] args)
+        {
+            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            Console.WriteLine("Client ready");
+
+            Stream ns = clientSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            Console.WriteLine("Indtast Personbil eller Elbil");
+
+            int count = 0;
+            while (count++ < 2)
+            {
+                string messageCarType = Console.ReadLine();
+                Console.WriteLine("Indtast pris (kun tal)");
+                string messagePrice = Console.ReadLine();
+
+                sw.WriteLine(messageCarType);
+                sw.WriteLine(messagePrice);
+
+                string serverAnswer = sr.ReadLine();
+                Console.WriteLine("Server: " + serverAnswer);
+            }
+
+            Console.WriteLine("No more from server. Press Enter");
+            Console.ReadKey();
+
+            ns.Close();
+            clientSocket.Close();
+        }
+    }
+}
---
+++ TCPServer/TCPServer/SwitchService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using Skat;
+
+namespace TCPServer
+{
+    class SwitchService
+    {
+        private TcpClient _connectionSocket;
+
+        public SwitchService(TcpClient connection)
+        {
+            _connectionSocket = connection;
+        }
+
+        public void DoIt()
+        {
+            try
+            {
+                Stream ns = _connectionSocket.GetStream();
+                StreamReader sr = new StreamReader(ns);
+                StreamWriter sw = new StreamWriter(ns);
+                sw.AutoFlush = true;
+                Afgift afgift = new Afgift();
+
+                string messageCarType = sr.ReadLine();
+                string messagePrice = sr.ReadLine();
+                int price = Convert.ToInt32(messagePrice);
+                string answer;
+
+                switch (messageCarType)
+                {
+                    case "Personbil":
+                    {
+                        string regAfgift = afgift.BilAfgift(price).ToString();
+                        answer = "Registreringsafgiften er: " + regAfgift;
+                        sw.WriteLine(answer);
+                        break;
+                    }
+                    case "Elbil":
+                    {
+                        string regAfgift = afgift.ElBilAfgift(price).ToString();
+                        answer = "Registreringsafgiften er: " + regAfgift;
+                        sw.WriteLine(answer);
+                        break;
+                    }
+                    default:
+                    {
+                        answer = "Du skal skrive Personbil eller Elbil";
+                        sw.WriteLine(answer);
+                        break;
+                    }
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format is not number", e.Message);
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Argument cant be null or empty", e.Message);
+            }
+        }
+    }
+}
